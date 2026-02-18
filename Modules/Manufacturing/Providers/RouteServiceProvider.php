88679389dbf17b5b2be026880f905b6ae8eee236
dhruvpwtech <dhruvraj.pwtech@gmail.com>
2026-02-18 22:30:55 +0530

    commit dhruv

--- Modules/Manufacturing/Providers/RouteServiceProvider.php
+++ Modules/Manufacturing/Providers/RouteServiceProvider.php
@@ -47,7 +47,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware(['web', 'common', 'auth', 'active'])
+        Route::middleware('web')
             ->namespace($this->moduleNamespace)
             ->group(module_path('Manufacturing', '/Routes/web.php'));
     }
